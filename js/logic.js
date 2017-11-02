5c736f1a14c26ed9c01b35af30aefc9eeec2fb9e
Yaroslav Voloshyn <jarik.voloshyn@gmail.com>
2017-11-02 17:12:50 +0200

    Create button to refresh news

--- js/logic.js
+++ js/logic.js
@@ -1,19 +1,24 @@
 $(document).ready(function() {
+  loadMainInfo();
+  });
+
+function loadMainInfo() {
   const $loading = $('div.loading');
   const $news    = $('div.news');
   const $heading = $('#heading');
 
+  $( ".newsText" ).remove();
   var dataLoading = $.get("http://content.guardianapis.com/search?api-key=test", function(data) {
       var news = data.response.results;
         for (var i = 0; i < news.length; i++) {
           var article = news[i];
-          $news.append('<p>' + article.webTitle + '</p>');
+          $news.append('<p class="newsText">' + article.webTitle + '</p>');
         }
         $loading.hide();
+        document.getElementById("refreshBtn").type  = "submit";
   }).fail(function() {
     $heading.hide();
     $loading.hide();
     document.getElementById("errImage").style.display = "block";
   });
-
-});
+}
