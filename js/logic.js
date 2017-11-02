f9e0a672ba9fd7a0526de90f82fb2f0e9cf0fbf9
Yaroslav Voloshyn <jarik.voloshyn@gmail.com>
2017-11-02 13:12:53 +0200

    Some new features

--- js/logic.js
+++ js/logic.js
@@ -1,12 +1,13 @@
 $(document).ready(function() {
+  const $loading = $('div.loading');
   const $news = $("div.news");
 
   $.get("http://content.guardianapis.com/search?api-key=test", function(data) {
-           var news = data.response.results;
-
-           for (var i = 0; i < news.length; i++) {
-             var article = news[i];
-             $news.append('<p>' + article.webTitle + '</p>');
-           }
-        });
+    var news = data.response.results;
+      for (var i = 0; i < news.length; i++) {
+        var article = news[i];
+        $news.append('<p>' + article.webTitle + '</p>');
+      }
+      $loading.hide()
+  });
 });
