86cb96e181af6c8ff0d1915314f9d49ea2839294
Yaroslav Voloshyn <jarik.voloshyn@gmail.com>
2017-11-09 22:28:10 +0200

    -

--- js/logic.js
+++ js/logic.js
@@ -7,18 +7,24 @@ function loadMainInfo() {
   const $news    = $('div.news');
   const $heading = $('#heading');
 
-  $( ".newsText" ).remove();
-  var dataLoading = $.get("http://content.guardianapis.com/search?api-key=test", function(data) {
+  $( ".news" ).empty();
+  $(".addInfo").hide();
+  $(".addInfo div").click(function() {
+      $(this).next("p").slideToggle("slow").siblings("p:visible")
+      .slideUp("slow");
+  });
+  var dataLoading = $.get("https://content.guardianapis.com/search?show-blocks=body&api-key=942512ab-eac1-48d3-bb94-c4360d852aa9", function(data) {
       var news = data.response.results;
         for (var i = 0; i < news.length; i++) {
           var article = news[i];
-          $news.append('<p class="newsText">' + article.webTitle + '</p>');
+          $news.append('<h5 class="newsText">' + '<a href="#">' + article.webTitle + '</a>' + '</h3>' +
+                       '<div class="addInfo">' + article.blocks.body[0].bodyHtml + '</div>');
         }
         $loading.hide();
         document.getElementById("refreshBtn").type  = "submit";
   }).fail(function() {
     $heading.hide();
     $loading.hide();
-    document.getElementById("errImage").style.display = "block";
+    $('errImage').style.display = "block";
   });
 }
