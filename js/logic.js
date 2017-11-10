1fd2c83961ac511844610dd057e4daca94baa591
Yaroslav Voloshyn <jarik.voloshyn@gmail.com>
2017-11-10 13:42:32 +0200

    Add sliding text

--- js/logic.js
+++ js/logic.js
@@ -1,5 +1,15 @@
 $(document).ready(function() {
   loadMainInfo();
+  $(document).on('click', function( event ) {
+     if( $(event.target).closest(".addInfo").length )
+     $(".addInfo").slideUp("slow");
+     event.stopPropagation();
+  });
+  $(document).on('click','.newsText',function( event ) {
+     $(this).siblings(".addInfo").slideToggle("slow");
+     $(this).css("background", "#e8e7e1");
+     return false;
+  });
   });
 
 function loadMainInfo() {
@@ -9,16 +19,17 @@ function loadMainInfo() {
 
   $( ".news" ).empty();
   $(".addInfo").hide();
-  $(".addInfo div").click(function() {
-      $(this).next("p").slideToggle("slow").siblings("p:visible")
-      .slideUp("slow");
-  });
   var dataLoading = $.get("https://content.guardianapis.com/search?show-blocks=body&api-key=942512ab-eac1-48d3-bb94-c4360d852aa9", function(data) {
       var news = data.response.results;
         for (var i = 0; i < news.length; i++) {
-          var article = news[i];
-          $news.append('<h5 class="newsText">' + '<a href="#">' + article.webTitle + '</a>' + '</h3>' +
-                       '<div class="addInfo">' + article.blocks.body[0].bodyHtml + '</div>');
+          var article = news[i],
+              additionalInfo =
+              (article.blocks.body[0].bodyTextSummary.split('\.')[0]) + "." +
+              (article.blocks.body[0].bodyTextSummary.split('\.')[1]) + "...";
+          $news.append('<div class="eachNews">' +
+                          '<div class=newsText>' + article.webTitle + '</div>'+
+                          '<div class=addInfo>'  + additionalInfo + '</div>' +
+                       '</div>');
         }
         $loading.hide();
         document.getElementById("refreshBtn").type  = "submit";
